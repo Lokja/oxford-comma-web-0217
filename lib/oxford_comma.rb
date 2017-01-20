cdd2f6874c6e3e958b4b559783a3b4c95bac8667
Devin Rieger <dev0089@gmail.com>
2017-01-20 20:39:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+array = []
 
+def oxford_comma(array)
+  if array.length == 1
+    final_string = array.join
+  elsif array.length == 2
+    final_string = array.join(" and ")
+  else
+    i = 0
+    final_string = ''
+    array.each do |string|
+      if i < array.length - 1
+        final_string << "#{string}, "
+        i += 1
+      else
+        final_string << "and #{string}"
+      end
+    end
+  end
+  final_string
 end
